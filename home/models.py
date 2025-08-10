987f0dcd8a74ad2104106d73a315e717217134db
Raj-128 <rajpatil052003@gmail.com>
2025-08-10 16:09:54 +0530

    27177

--- home/models.py
+++ home/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+class Restaurant(models.Model):
+    name = models.Charfield(max_length=255)
+
+    def_str(self):
+        return self.name
