372ac492a2c4d64a653cd264d2af83a3d820fb2c
Raj-128 <rajpatil052003@gmail.com>
2025-08-16 21:45:56 +0530

    32893

--- account/models.py
+++ account/models.py
@@ -4,14 +4,14 @@ from django.db.models.signals import post_save
 from django.dispatch import receiver
 # Create your models here.
 class UserProfile(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE,related_name="profile")
-    phone_number = models.CharField(max_length=15, blank=True,nnull=True)
+    user = models.OneToOneField(User,on_delete=models.CASCADE,related_name="profile")
+    phone_number = models.CharField(max_length=15,blank=True,null=True)
 
     def __str__(self):
         return self.user.username
 
     @receiver(post_save, sender=User)
-    def create_or_update_user_profile(sender, instance,created, **kwargs):
+    def create_or_update_user_profile(sender,instance,created,**kwargs):
         if created:
             UserProfile.objects.create(user=instance)
         else:
