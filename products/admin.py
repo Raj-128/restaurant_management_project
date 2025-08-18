7624be29ec7b0358c79696b4729f04f5b77054d4
Raj-128 <rajpatil052003@gmail.com>
2025-08-18 20:37:08 +0530

    34206

--- products/admin.py
+++ products/admin.py
@@ -3,10 +3,4 @@ from .models import *
 
 
 # Custom Admins
-class ItemAdmin(admin.ModelAdmin):
-    list_display = ['item_name','item_price','created_at']
-
-
-# Register your models here.
-admin.site.register(Item,ItemAdmin)
-admin.site.register(Menu)
+admin.siteregister(MenuItem)
