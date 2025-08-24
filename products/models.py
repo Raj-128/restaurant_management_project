8b9f999676c402ef19ee16eeebd1d9053a9c20b4
Raj-128 <rajpatil052003@gmail.com>
2025-08-24 16:26:25 +0530

    38224

--- products/models.py
+++ products/models.py
@@ -6,5 +6,7 @@ class MenuItem(models.Model):
     description = models.TextField(blank=True,null=True)
     price = models.DecimalField(max_digits=10, decimal_places=2)
 
+    image = models.ImageField(upload_to="menu_images/", blank=True, null=True)
+    
     def __str__(self):
         return str(self.name)
