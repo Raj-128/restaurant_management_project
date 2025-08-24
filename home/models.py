e121fa5a3945b6dd7d83b3af5d31c5682f5e1050
Raj-128 <rajpatil052003@gmail.com>
2025-08-24 16:15:54 +0530

    38226

--- home/models.py
+++ home/models.py
@@ -8,6 +8,31 @@ class Restaurant(models.Model):
     def __str__(self):
         return self.name
 
+class Restaurantlocation(models.Model):
+    address = models.CharField(max_length=255)
+    city = models.CharField(max_length=100)
+    state = models.CharField(max_length=100)
+    zip_code = models.CharField(max_length=20)
+
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def _str__(self):
+        returnf"{self.address},{self.city},{self.state} - {self.zip_code}"
+
+class ContactSubmission(models.Model):
+    name = models.CharField(max_length=100)
+    email = models.EmailField() 
+    message = models.TextField(blank=True,null=True)
+    created_at = models.DateTimeField(auto_now_add=True) 
+
+    class Meta:
+        verbose_name = "Contact Submission "
+        verbose_name_plural = "Contact Submissions"
+
+        def __str__(self):
+            return f"{self.name} - {self.email}"
+
 
 class Feedback(models.Model):
     name = models.CharField(max_length=200)
