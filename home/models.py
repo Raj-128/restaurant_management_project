8b7a88bbfe396c434bbcdf4bda504da2c805964b
Raj-128 <rajpatil052003@gmail.com>
2025-08-26 20:05:58 +0530

    38542

--- home/models.py
+++ home/models.py
@@ -3,7 +3,10 @@ from django.db import models
 class Restaurant(models.Model):
     name = models.CharField(max_length=255)
     phone_number = models.CharField(max_length=15)
+    address = models.TextField(blank=True, null=True)
 
+    opening_hours = models.JSONField(default=dict,blank=True, null=True)
+    
     def __str__(self):
         return self.name
 
